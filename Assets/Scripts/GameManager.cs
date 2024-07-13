1b76138abbdfaddca9335f0b1e62ae0fc1f43d21
pegasys <sconnolly7@outlook.com>
2024-07-14 00:44:45 +0100

    respawn system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    private CheckpointRoom currentCheckpoint;
+
     private void Awake()
     {
         if (Instance == null)
@@ -21,6 +23,17 @@ public class GameManager : MonoBehaviour
 
     public static void OnPlayerDeath()
     {
-        Debug.Log("PLAYER DIED");
+        Debug.Log("PLAYER DIED > RESETTING CHECKPOINT");
+        Instance.currentCheckpoint.Reset();
+    }
+
+    public static void OnCheckpointEnter(CheckpointRoom checkpointRoom)
+    {
+        // ignore earlier checkpoints
+        if (Instance.currentCheckpoint != null && checkpointRoom.Id <= Instance.currentCheckpoint.Id)
+            return;
+
+        Instance.currentCheckpoint = checkpointRoom;
+        Debug.Log($"CHECKPOINT UPDATED [{checkpointRoom.Id}]");
     }
 }
