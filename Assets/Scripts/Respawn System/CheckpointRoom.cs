2549caedeca2d560116d09d1305a6141771ece7c
pegasys <sconnolly7@outlook.com>
2024-07-13 22:18:41 +0100

    added skybox and started on respawn system

---
+++ Assets/Scripts/Respawn System/CheckpointRoom.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointRoom : MonoBehaviour
+{
+    public Transform spawnTransform;
+
+    public List<Enemy> associatedEnemies = new List<Enemy>();
+}
