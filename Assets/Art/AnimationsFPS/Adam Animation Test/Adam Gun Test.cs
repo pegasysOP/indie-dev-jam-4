5493dc7dce1cc37b43135b90be0d3410fb1f94d0
AdamF <adam.farrant21@gmail.com>
2024-11-18 11:36:33 +0000

    More animations!

--- Assets/Art/AnimationsFPS/Adam Animation Test/Adam Gun Test.cs
+++ Assets/Art/AnimationsFPS/Adam Animation Test/Adam Gun Test.cs
@@ -1,195 +1,152 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class AnimatorController : MonoBehaviour
 {
-    public Animator armsAnimator;
-    public Animator pistolAnimator;
-    public GameObject gunParticleEffectsParent;
-    public AudioSource gunAudioSource;
+   [Header("Animation")]
+   [SerializeField] private Animator armsAnimator;  // Animator for arms
+   [SerializeField] private Animator pistolAnimator; // Animator for pistol
+   
+   [Header("Animation Layers")]
+   [SerializeField] private float ironSightsLayerWeight = 0f;
+   [SerializeField] private float ironSightsTransitionSpeed = 10f;
+   
+   [Header("Key Bindings")]
+   [SerializeField] private KeyCode inspectKey = KeyCode.Alpha1;
+   [SerializeField] private KeyCode meleeKey = KeyCode.Alpha2;
+   [SerializeField] private KeyCode fireKey = KeyCode.Alpha3;
+   [SerializeField] private KeyCode reloadKey = KeyCode.Alpha4;
+   [SerializeField] private KeyCode reloadEmptyKey = KeyCode.Alpha5;
+   [SerializeField] private KeyCode reloadSwapKey = KeyCode.Alpha6;
+   [SerializeField] private KeyCode ironSightsKey = KeyCode.Mouse1;
 
-    void Update()
-    {
-        // Handle melee animation
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            if (armsAnimator != null)
-            {
-                armsAnimator.SetTrigger("Melee_T");
-                Debug.Log("Arms melee triggered");
-            }
-            else
-            {
-                Debug.LogWarning("Arms Animator is not assigned!");
-            }
+   [Header("Aim Settings")]
+   [SerializeField] private float aimTransitionSpeed = 10f;
+   [SerializeField] private float currentAimWeight = 0f;
+   
+   [Header("Effects")]
+   [SerializeField] private GameObject gunParticleEffectsParent;
+   [SerializeField] private AudioSource gunAudioSource;
 
-            if (pistolAnimator != null)
-            {
-                pistolAnimator.SetTrigger("Melee_T");
-                Debug.Log("Pistol melee triggered");
-            }
-            else
-            {
-                Debug.LogWarning("Pistol Animator is not assigned!");
-            }
-        }
+   private Dictionary<KeyCode, string> animationTriggers;
 
-        // Handle inspect animation
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            if (armsAnimator != null)
-            {
-                armsAnimator.SetTrigger("Inspect_T");
-                Debug.Log("Arms inspect triggered");
-            }
-            else
-            {
-                Debug.LogWarning("Arms Animator is not assigned!");
-            }
+   private void Awake()
+   {
+       // Initialize dictionary with serialized keys
+       animationTriggers = new Dictionary<KeyCode, string>
+       {
+           { inspectKey, "Inspect_T" },
+           { meleeKey, "Melee_T" },
+           { fireKey, "Fire_T" },
+           { reloadKey, "Reload_T" },
+           { reloadEmptyKey, "ReloadEMPTY_T" },
+           { reloadSwapKey, "ReloadSWAP_T" }
+       };
+   }
 
-            if (pistolAnimator != null)
-            {
-                pistolAnimator.SetTrigger("Inspect_T");
-                Debug.Log("Pistol inspect triggered");
-            }
-            else
-            {
-                Debug.LogWarning("Pistol Animator is not assigned!");
-            }
-        }
+   private void Update()
+   {
+       HandleWalking();
+       HandleAiming();
+       HandleAnimationTriggers();
+   }
 
-        // Handle fire animation, sound, and particle effects
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            if (armsAnimator != null)
-            {
-                armsAnimator.SetTrigger("Fire_T");
-                Debug.Log("Fire_T set for arms");
-            }
-            else
-            {
-                Debug.LogWarning("Arms Animator is not assigned!");
-            }
+   private void HandleWalking()
+   {
+       bool isWalking = Input.GetKey(KeyCode.W);
 
-            if (pistolAnimator != null)
-            {
-                pistolAnimator.SetTrigger("Fire_T");
-                Debug.Log("Fire_T set for pistol");
+       // Set walking animation in both the arms and pistol Animator
+       if (armsAnimator != null)
+       {
+           armsAnimator.SetBool("IsWalking", isWalking);
+       }
+       if (pistolAnimator != null)
+       {
+           pistolAnimator.SetBool("IsWalking", isWalking);
+       }
+   }
 
-                // Play gun sound
-                PlayGunSound();
+   private void HandleAiming()
+   {
+       // Target weight is 1 when aiming, 0 when not
+       float targetWeight = Input.GetKey(ironSightsKey) ? 1f : 0f;
+       
+       // Smoothly interpolate between current and target weight
+       currentAimWeight = Mathf.Lerp(currentAimWeight, targetWeight, Time.deltaTime * aimTransitionSpeed);
 
-                // Play particle effects
-                PlayGunParticleEffects();
-            }
-            else
-            {
-                Debug.LogWarning("Pistol Animator is not assigned!");
-            }
-        }
+       // Apply the smoothed weight to the animator
+       if (armsAnimator != null)
+       {
+           armsAnimator.SetFloat("AimWeight", currentAimWeight);
+       }
+   }
 
-        // Handle reload animation
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            if (armsAnimator != null)
-            {
-                armsAnimator.SetTrigger("Reload_T");
-                Debug.Log("Arms reload triggered");
-            }
-            else
-            {
-                Debug.LogWarning("Arms Animator is not assigned!");
-            }
+   private void HandleAnimationTriggers()
+   {
+       foreach (var trigger in animationTriggers)
+       {
+           if (Input.GetKeyDown(trigger.Key))
+           {
+               PlayAnimation(trigger.Value);
+               
+               if (trigger.Key == fireKey)
+               {
+                   HandleFireEffects();
+               }
+               break;
+           }
+       }
+   }
 
-            if (pistolAnimator != null)
-            {
-                pistolAnimator.SetTrigger("Reload_T");
-                Debug.Log("Pistol reload triggered");
-            }
-            else
-            {
-                Debug.LogWarning("Pistol Animator is not assigned!");
-            }
-        }
+   private void PlayAnimation(string triggerName)
+   {
+       TryTriggerAnimation(armsAnimator, triggerName, "Arms");
+       TryTriggerAnimation(pistolAnimator, triggerName, "Pistol");
+   }
 
-        // Handle empty reload animation
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            if (armsAnimator != null)
-            {
-                armsAnimator.SetTrigger("ReloadEMPTY_T");
-                Debug.Log("Arms empty reload triggered");
-            }
-            else
-            {
-                Debug.LogWarning("Arms Animator is not assigned!");
-            }
+   private void TryTriggerAnimation(Animator animator, string triggerName, string animatorName)
+   {
+       if (animator != null)
+       {
+           animator.SetTrigger(triggerName);
+           Debug.Log($"{animatorName} {triggerName} triggered");
+       }
+       else
+       {
+           Debug.LogWarning($"{animatorName} Animator is not assigned!");
+       }
+   }
 
-            if (pistolAnimator != null)
-            {
-                pistolAnimator.SetTrigger("ReloadEMPTY_T");
-                Debug.Log("Pistol empty reload triggered");
-            }
-            else
-            {
-                Debug.LogWarning("Pistol Animator is not assigned!");
-            }
-        }
+   private void HandleFireEffects()
+   {
+       PlayGunSound();
+       PlayGunParticleEffects();
+   }
 
-        // Handle swap reload animation
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            if (armsAnimator != null)
-            {
-                armsAnimator.SetTrigger("ReloadSWAP_T");
-                Debug.Log("Arms swap reload triggered");
-            }
-            else
-            {
-                Debug.LogWarning("Arms Animator is not assigned!");
-            }
+   private void PlayGunSound()
+   {
+       if (gunAudioSource != null)
+       {
+           gunAudioSource.Play();
+           Debug.Log("Gun sound played");
+           return;
+       }
+       Debug.LogWarning("Gun AudioSource is not assigned!");
+   }
 
-            if (pistolAnimator != null)
-            {
-                pistolAnimator.SetTrigger("ReloadSWAP_T");
-                Debug.Log("Pistol swap reload triggered");
-            }
-            else
-            {
-                Debug.LogWarning("Pistol Animator is not assigned!");
-            }
-        }
-    }
+   private void PlayGunParticleEffects()
+   {
+       if (gunParticleEffectsParent == null)
+       {
+           Debug.LogWarning("Gun Particle Effects parent is not assigned!");
+           return;
+       }
 
-    void PlayGunSound()
-    {
-        if (gunAudioSource != null)
-        {
-            gunAudioSource.Play();
-            Debug.Log("Gun sound played");
-        }
-        else
-        {
-            Debug.LogWarning("Gun AudioSource is not assigned!");
-        }
-    }
-
-    void PlayGunParticleEffects()
-    {
-        if (gunParticleEffectsParent != null)
-        {
-            ParticleSystem[] particleSystems = gunParticleEffectsParent.GetComponentsInChildren<ParticleSystem>();
-            
-            foreach (ParticleSystem ps in particleSystems)
-            {
-                ps.Play();
-                Debug.Log("Playing particle system: " + ps.name);
-            }
-        }
-        else
-        {
-            Debug.LogWarning("Gun Particle Effects parent is not assigned!");
-        }
-    }
+       ParticleSystem[] particleSystems = gunParticleEffectsParent.GetComponentsInChildren<ParticleSystem>();
+       foreach (ParticleSystem ps in particleSystems)
+       {
+           ps.Play();
+           Debug.Log($"Playing particle system: {ps.name}");
+       }
+   }
 }
